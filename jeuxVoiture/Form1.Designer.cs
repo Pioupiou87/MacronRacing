8e7ef37813879872bfa35d6aaab82fff9b24abbe
PONCHEL Mickael <F015189@gefco.net>
2018-04-16 17:23:17 +0200

    premier pas

--- jeuxVoiture/Form1.Designer.cs
+++ jeuxVoiture/Form1.Designer.cs
@@ -28,13 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.voiture = new System.Windows.Forms.PictureBox();
+            this.macron = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.voiture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.macron)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // voiture
+            // 
+            this.voiture.Image = ((System.Drawing.Image)(resources.GetObject("voiture.Image")));
+            this.voiture.Location = new System.Drawing.Point(135, 55);
+            this.voiture.Name = "voiture";
+            this.voiture.Size = new System.Drawing.Size(100, 50);
+            this.voiture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.voiture.TabIndex = 0;
+            this.voiture.TabStop = false;
+            // 
+            // macron
+            // 
+            this.macron.Image = ((System.Drawing.Image)(resources.GetObject("macron.Image")));
+            this.macron.Location = new System.Drawing.Point(397, 12);
+            this.macron.Name = "macron";
+            this.macron.Size = new System.Drawing.Size(70, 118);
+            this.macron.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.macron.TabIndex = 1;
+            this.macron.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(283, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(520, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.voiture);
+            this.Controls.Add(this.macron);
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            ((System.ComponentModel.ISupportInitialize)(this.voiture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.macron)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox voiture;
+        private System.Windows.Forms.PictureBox macron;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- jeuxVoiture/Form1.cs
+++ jeuxVoiture/Form1.cs
@@ -16,5 +16,44 @@ namespace jeuxVoiture
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                voiture.Location = new Point(voiture.Location.X - 10, voiture.Location.Y);
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                voiture.Location = new Point(voiture.Location.X + 10, voiture.Location.Y);
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                voiture.Location = new Point(voiture.Location.X, voiture.Location.Y - 10);
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                voiture.Location = new Point(voiture.Location.X, voiture.Location.Y + 10);
+            }
+            if (e.KeyCode == Keys.Up && e.KeyCode == Keys.Right)
+            {
+                voiture.Location = new Point(voiture.Location.X + 10, voiture.Location.Y - 10);
+            }
+
+
+        }
+        private void percute ()
+        {
+            if (voiture.Location == macron.Location)
+            {
+                this.Text += "Hello World!"; 
+            }
+
+        }
     }
-}
+ }
+
